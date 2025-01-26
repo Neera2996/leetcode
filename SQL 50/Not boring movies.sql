986a5328e537b7e8fde35b19db241da8424d9620
Neera2996 <67223874+Neera2996@users.noreply.github.com>
2025-01-27 01:42:07 +0530

    Not boring movies

---
+++ SQL 50/Not boring movies.sql
@@ -0,0 +1,21 @@
+/*
+Table: Cinema
+
++----------------+----------+
+| Column Name    | Type     |
++----------------+----------+
+| id             | int      |
+| movie          | varchar  |
+| description    | varchar  |
+| rating         | float    |
++----------------+----------+
+id is the primary key (column with unique values) for this table.
+Each row contains information about the name of a movie, its genre, and its rating.
+rating is a 2 decimal places float in the range [0, 10]
+*/
+
+# Write your MySQL query statement below
+
+SELECT * from Cinema where
+(id%2!=0) and description!='boring'
+Order By rating desc
